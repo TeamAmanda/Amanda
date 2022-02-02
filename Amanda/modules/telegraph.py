94b95fe5a50d0af62ec0e61aa9d19a0aeb7781fd
Tharuk Renuja <90763454+TharukRenuja@users.noreply.github.com>
2022-02-02 18:40:11 +0530

    Update telegraph.py

--- Amanda/modules/telegraph.py
+++ Amanda/modules/telegraph.py
@@ -1,50 +1,87 @@
 import os
-
-from pyrogram import filters
-from telegraph import upload_file
-
-from Amanda import pbot as app
+from Amanda.config import Config
+from Amanda.events import register
 from Amanda import dispatcher
+from Amanda import telethn
+from Amanda import TEMP_DOWNLOAD_DIRECTORY as TMP_DOWNLOAD_DIRECTORY
+from telethon import events
+from PIL import Image
+from datetime import datetime
+from telegraph import Telegraph, upload_file, exceptions
+babe = "RoseLoverX"
+telegraph = Telegraph()
+r = telegraph.create_account(short_name=babe)
+auth_url = r["auth_url"]
 
-@app.on_message(filters.command("telegraph"))
-async def telegraph(client, message):
-    replied = message.reply_to_message
-    if not replied:
-        await message.reply("Reply to a supported media file")
+@register(pattern="^/t(m|t) ?(.*)")
+async def _(event):
+    if event.fwd_from:
         return
-    if not (
-        (replied.photo and replied.photo.file_size <= 5242880)
-        or (replied.animation and replied.animation.file_size <= 5242880)
-        or (
-            replied.video
-            and replied.video.file_name.endswith(".mp4")
-            and replied.video.file_size <= 5242880
-        )
-        or (
-            replied.document
-            and replied.document.file_name.endswith(
-                (".jpg", ".jpeg", ".png", ".gif", ".mp4"),
+    optional_title = event.pattern_match.group(2)
+    if event.reply_to_msg_id:
+        start = datetime.now()
+        r_message = await event.get_reply_message()
+        input_str = event.pattern_match.group(1)
+        if input_str == "m":
+            downloaded_file_name = await tbot.download_media(
+                r_message,
+                TMP_DOWNLOAD_DIRECTORY
             )
-            and replied.document.file_size <= 5242880
-        )
-    ):
-        await message.reply("Not supported!")
-        return
-    download_location = await client.download_media(
-        message=message.reply_to_message,
-        file_name="root/downloads/",
-    )
-    try:
-        response = upload_file(download_location)
-    except Exception as document:
-        await message.reply(message, text=document)
+            end = datetime.now()
+            ms = (end - start).seconds
+            h = await event.reply("Downloaded to {} in {} seconds.".format(downloaded_file_name, ms))
+            if downloaded_file_name.endswith((".webp")):
+                resize_image(downloaded_file_name)
+            try:
+                start = datetime.now()
+                media_urls = upload_file(downloaded_file_name)
+            except exceptions.TelegraphException as exc:
+                await h.edit("ERROR: " + str(exc))
+                os.remove(downloaded_file_name)
+            else:
+                end = datetime.now()
+                ms_two = (end - start).seconds
+                os.remove(downloaded_file_name)
+                await h.edit("Uploaded to https://telegra.ph{} in {} seconds.".format(media_urls[0], (ms + ms_two)), link_preview=True)
+        elif input_str == "t":
+            user_object = await tbot.get_entity(r_message.sender_id)
+            title_of_page = user_object.first_name # + " " + user_object.last_name
+            # apparently, all Users do not have last_name field
+            if optional_title:
+                title_of_page = optional_title
+            page_content = r_message.message
+            if r_message.media:
+                if page_content != "":
+                    title_of_page = page_content
+                downloaded_file_name = await tbot.download_media(
+                    r_message,
+                    TMP_DOWNLOAD_DIRECTORY
+                )
+                m_list = None
+                with open(downloaded_file_name, "rb") as fd:
+                    m_list = fd.readlines()
+                for m in m_list:
+                    page_content += m.decode("UTF-8") + "\n"
+                os.remove(downloaded_file_name)
+            page_content = page_content.replace("\n", "<br>")
+            response = telegraph.create_page(
+                title_of_page,
+                html_content=page_content
+            )
+            end = datetime.now()
+            ms = (end - start).seconds
+            await event.reply("Pasted to https://telegra.ph/{} in {} seconds.".format(response["path"], ms), link_preview=True)
     else:
-        await message.reply(
-            f"**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}**",
-            disable_web_page_preview=True,
-        )
-    finally:
-        os.remove(download_location)
+        await event.reply("Reply to a message to get a permanent telegra.ph link.")
+
+
+def resize_image(image):
+    im = Image.open(image)
+    im.save(image, "PNG")
+
+file_help = os.path.basename(__file__)
+file_help = file_help.replace(".py", "")
+file_helpo = file_help.replace("_", " ")
 
 TELEGRAPH_HANDLER = DisableAbleCommandHandler("telegraph", tgh)
 
