76d772ab17b271e1c3c69a5c71f4ec33489f5dbb
Tharuk Renuja <90763454+TharukRenuja@users.noreply.github.com>
2022-02-03 08:50:31 +0530

    Update __init__.py

--- Amanda/modules/sql/__init__.py
+++ Amanda/modules/sql/__init__.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
-#from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, Sequence, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import event
 
